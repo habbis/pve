f105bd05e78ee1fc922c5b2ddecc16d7bfad827a
habbis <habbis@habbestad.org>
2022-01-16 23:15:04 +0000

    added sshkeys class to pve

---
+++ manifests/sshkeys.pp
@@ -0,0 +1,30 @@
+class pve::sshkeys (
+  $puppet_homelab_sshkeys_ssh_authorized_keys = puppet_homelab_sshkeys_ssh_authorized_keys) {
+  if $kernel == "Linux" {
+    file { '/root/.ssh/authorized_keys':
+      ensure  => file,
+      backup  => false,
+      content => template("puppet_homelab/sshkeys/authorized_keys.erb"),
+    }
+    file { '/root/.ssh': 
+      ensure  => directory,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0700';
+    }
+  }
+
+  if $kernel == "Linux" {
+    file { '/home/ansible/.ssh/authorized_keys':
+      ensure  => file,
+      backup  => false,
+      content => template("puppet_homelab/sshkeys/authorized_keys.erb"),
+    }
+    file { '/home/ansible/.ssh': 
+      ensure  => directory,
+      owner   => 'ansible',
+      group   => 'ansible',
+      mode    => '0700';
+    }
+  }
+}
